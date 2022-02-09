67fa48dd57db55d9f61c7dc7c7de053aad323a8e
Dionysis Lekkas <dlekkas@novibet.com>
2022-02-09 12:55:56 +0200

    NoteList component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoteListComponent } from './pages/note-list/note-list.component';
 
 const routes: Routes = [
-  
+  {path: '', component: NoteListComponent}
 ];
 
 @NgModule({
